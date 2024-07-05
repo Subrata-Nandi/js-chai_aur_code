417eebeb0294c65f7b4f1f34785dc15e5e8399e8
Subrata nandi <105857915+Subrata-Nandi@users.noreply.github.com>
2024-07-05 10:04:13 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // We are using nodejs, not browser
+
+// code readability should be high
+
+// ECMAScript tc39.es and mdn documentation
+
+let name = "Subrata"
+let age = 22
+let isLoggedIn = false
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value is assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Subrata");
+console.log(typeof age);
+console.log(typeof null);   // null is a object
+console.log(typeof undefined);  // undefined
