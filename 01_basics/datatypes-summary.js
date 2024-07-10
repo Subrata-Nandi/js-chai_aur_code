7508a2d16b97724e0b849c9e2e289b44032563c7
Subrata nandi <105857915+Subrata-Nandi@users.noreply.github.com>
2024-07-10 19:35:05 +0000

    Datatype summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -9,11 +9,11 @@
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId)
+// console.log(id === anotherId)
 
 const bigNumber = 123124123451543235435n
 
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
 // typeof null is object
 // typeof undefined is undefined
@@ -26,5 +26,30 @@ const myFunction = function() {
     console.log("Hello World");
 }
 
-myFunction()
-console.log(typeof myFunction);
+// myFunction()
+// console.log(typeof myFunction);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myName = "Subrata"
+
+let anotherName = myName
+anotherName = "Raja"
+
+console.log(myName);
+console.log(anotherName);  // Here you will get a copy
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne  // Here you will get reference
+
+userTwo.email = "user@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
