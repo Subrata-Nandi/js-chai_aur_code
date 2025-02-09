061225ad68a3276754e517ae6c6a8138723973e9
Subrata nandi <105857915+Subrata-Nandi@users.noreply.github.com>
2025-02-09 20:03:46 +0000

    object2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -8,4 +8,47 @@ user.isLoggedIn = false
 
 // console.log(user);
 
+const user2 = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "subrata",
+            lastname: "nandi"
+        }
+    }
+}
+
+// console.log(user2.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+// const obj3 = {obj1, obj2}
+// console.log(obj3);
+
+// const obj3 = Object.assign(obj1, obj2) // Everything is copied to obj1 and also stored in obj3
+// So its better to use an empty target object
+
+// const obj3 = Object.assign({}, obj1, obj2)
+
+// Best way is to use spread operator
+const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3);
+// console.log(obj1);
+
+console.log(user);
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
+
+console.log(user.hasOwnProperty('name'));
+console.log(user.hasOwnProperty('naming'));
+
+
+
+
+
+
+
+
 
