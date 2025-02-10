1816f7dc4d321c9a1aa6d754a5a5fa9d68bb32cd
Subrata nandi <105857915+Subrata-Nandi@users.noreply.github.com>
2025-02-10 13:52:40 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,18 @@
+var c = 300
+
+if(true) {
+    let a = 1
+    const b = 2
+    var c = 3
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c); // This is a problem of using var, block scope problem
+
+// block scope and global scope
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
