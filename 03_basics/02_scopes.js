96ac3d3df72395fb4e20bc8a0143270d18c2f8a7
Subrata nandi <105857915+Subrata-Nandi@users.noreply.github.com>
2025-02-10 20:12:46 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,11 +8,41 @@ if(true) {
 
 // console.log(a);
 // console.log(b);
-console.log(c); // This is a problem of using var, block scope problem
+// console.log(c); // This is a problem of using var, block scope problem
 
 // block scope and global scope
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
+// }
+
+// nested function and scope
+function one() {
+    const username = "subrata"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
 }
+
+one()
+
+// ************************** Interesting *******************************
+
+addOne(5)
+
+function addOne(value) {
+    return value + 1
+}
+
+
+addTwo(5)   // Problem if called before
+
+const addTwo = function(num) {
+    return num + 2
+}
+
