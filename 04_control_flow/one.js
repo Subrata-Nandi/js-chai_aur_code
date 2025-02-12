8f99f0f0b2c27ebbf271236c02bc26aadcd4e036
Subrata nandi <105857915+Subrata-Nandi@users.noreply.github.com>
2025-02-12 13:10:30 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,32 @@
+// if
+const isLoggedIn = true
+
+if(2 == "2") {
+    console.log("executed");
+}
+
+if(2 === "2") console.log("executed")
+
+// >, <, >=, <=, ==, !=, ===, !==
+
+if (condition) {
+    
+} else {
+    
+}
+
+
+if(condition) {
+
+}
+else if(condition) {
+
+}
+else if(condition) {
+
+}
+else {
+
+}
+
+// && ||
