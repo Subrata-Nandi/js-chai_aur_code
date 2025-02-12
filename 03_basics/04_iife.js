a6cd70e38fa8eb9ea93a3000926f797dd99d331f
Subrata nandi <105857915+Subrata-Nandi@users.noreply.github.com>
2025-02-12 11:16:59 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,8 +1,18 @@
-// Immediately Invoked Function Expressions (IIFE)
+// Immediately Invoked Function Expressions (IIFE) => The function that immediately gets executed
 
 (function chai() {
+    // named iife      
     console.log(`DB CONNECTED`); 
-})()
+})();
 
 // Sometimes there is pollution from global scope declarations, so to avoid this we use iife
 
+
+( () => {   // using arrow function
+    console.log(`DB CONNECTED TWO`);
+} )();      // ';' is important for iife
+
+
+( (name) => {       // Parameter passing
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('Subrata');      
