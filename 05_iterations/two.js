d5e7a230e3e2f4a4e7d16eb1eaae43197fa32c22
Subrata nandi <105857915+Subrata-Nandi@users.noreply.github.com>
2025-02-14 06:42:07 +0000

    while

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+// let index = 1
+// while (index <= 5) {
+//     console.log(index);
+//     index++;
+// }
+
+let myArr = [1,2,3,4,5]
+// let i = 0
+// while(i < myArr.length) {
+//     console.log(myArr[i]);
+//     i++
+// }
+
+
+let score = 1
+do {
+    console.log(score);
+    score++
+} while (score <= 5);
